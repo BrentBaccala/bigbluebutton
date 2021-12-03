ced7d423f580d19ca0b457ab2f944e864cbfbe99
Brent Baccala <cosine@freesoft.org>
2021-12-03 01:13:06 -0500

    first crack at a script to run on a test SERVER

---
+++ bigbluebutton-tests/testserver.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#
+# Install Big Blue Button testing server
+
+# I put an http proxy in cloud-init, but it doesn't save that configuration between
+# boots.  They only way to get cloud-init to set the proxy is to provide a CD-ROM image.
+# Just running cloud-init with "policy: enabled" isn't enough.
+
+echo 'Acquire::http::Proxy "http://osito.freesoft.org:3128/";' | sudo tee /etc/apt/apt.conf.d/proxy.conf
+
+# if this is running, our apt operations may error out unable to get a lock
+sudo systemctl stop unattended-upgrades.service
+
+sudo apt update
+sudo DEBIAN_FRONTEND=noninteractive apt -y upgrade
+
+# the ubuntu.py script left some cruft owned by root
+sudo chown -R ubuntu.ubuntu /home/ubuntu
+
+# make a new certificate authority
+sudo DEBIAN_FRONTEND=noninteractive apt -y install easy-rsa
+make-cadir ca
+cd ca
+cp openssl-1.0.0.cnf openssl.cnf
+sed -i '/cRLSign/asubjectAltName = DNS:freesoft.org' openssl.cnf
+sed -i '/cRLSign/akeyUsage = critical, cRLSign, digitalSignature, keyCertSign' openssl.cnf
+. vars
+./clean-all
+
+# create the CA certificate (the cp/update at least makes GET happy)
+./pkitool --initca
+sudo cp keys/ca.crt /usr/local/share/ca-certificates
+sudo update-ca-certificates 
+
+# create the server certificate and put everything where the bbb install scripts wants it
+# (might want to actually create a full chain, which this currently isn't)
+./pkitool --server test.freesoft.org
+sudo mkdir -p /local/certs
+sudo cp keys/test.freesoft.org.crt /local/certs/fullchain.pem
+sudo cp keys/test.freesoft.org.key /local/certs/privkey.pem
+sudo cp keys/ca.crt /local/certs/ca.crt
+
+cd
+
+sudo sed -i '1s/localhost/localhost test.freesoft.org/' /etc/hosts
+
+# or bionic-230-dev
+# suggested -w: firewall
+# suggested -a: api demos
+wget -qO- https://ubuntu.bigbluebutton.org/bbb-install.sh | sudo bash -s -- -v bionic-23 -s test.freesoft.org -d
+
+# nginx won't start without this change
+sudo sed -i '/server_names_hash_bucket_size/s/^\(\s*\)# /\1/' /etc/nginx/nginx.conf
+
+#wget -qO- https://ubuntu.bigbluebutton.org/bbb-install.sh | sudo bash -s -- -v bionic-23 -s test.freesoft.org -d
+sudo systemctl start nginx
+
+# try to get these browsers to accept the self-signed CA
+
+# this works for firefox
+sudo apt install libnss3-tools
+firefox --headless --new-tab "javascript:top.window.close()"
+cat ca/keys/ca.crt | certutil -A -t C -d sql:$(echo .mozilla/firefox/*.default-release/) -n test
+# certutil -L -d sql:$(echo .mozilla/firefox/*.default-release/)
